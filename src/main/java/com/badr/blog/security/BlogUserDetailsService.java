b6c7d13e792735f56cbc1af17ff4bb0a7318d0c7
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2025-05-21 10:49:41 +0100

    Loading Users

---
+++ src/main/java/com/badr/blog/security/BlogUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.badr.blog.security;
+
+import com.badr.blog.domain.entities.User;
+import com.badr.blog.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class BlogUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("User not found with email: " + email)
+        );
+
+        return new BlogUserDetails(user);
+    }
+}
