3911b24a52b0910f5c9468c39c19b1f72098cb37
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2025-05-21 11:11:22 +0100

    Auth Endpoint

---
+++ src/main/java/com/badr/blog/services/AuthenticationService.java
@@ -0,0 +1,8 @@
+package com.badr.blog.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface AuthenticationService {
+    UserDetails authenticate(String email, String password);
+    String generateJwtToken(UserDetails userDetails);
+}
