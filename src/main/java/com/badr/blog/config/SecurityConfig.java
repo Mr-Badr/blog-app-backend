ff0016a06634c2e577b22009f69cf39fb83cfdee
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2025-05-24 22:41:29 +0100

    Add JwtAuthentication

--- src/main/java/com/badr/blog/config/SecurityConfig.java
+++ src/main/java/com/badr/blog/config/SecurityConfig.java
@@ -1,5 +1,10 @@
 package com.badr.blog.config;
 
+import com.badr.blog.domain.entities.User;
+import com.badr.blog.repositories.UserRepository;
+import com.badr.blog.security.BlogUserDetailsService;
+import com.badr.blog.security.JwtAuthenticationFilter;
+import com.badr.blog.services.AuthenticationService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,17 +12,46 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationService authenticationService) {
+        return new JwtAuthenticationFilter(authenticationService);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(UserRepository userRepository) {
+        BlogUserDetailsService blogUserDetailsService = new BlogUserDetailsService(userRepository);
+
+        // Create a user if he did not exist in our DB (Shouldn't be in prod)
+        String email = "badr@gmail.com";
+        userRepository.findByEmail(email).orElseGet(()->{
+            User newUser = User.builder()
+                    .name("Badr")
+                    .email(email)
+                    .password(passwordEncoder().encode("pass"))
+                    .build();
+            userRepository.save(newUser);
+            return newUser;
+        });
+
+        return blogUserDetailsService;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/api/v1/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/posts/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/categories/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/v1/tags/**").permitAll()
@@ -26,7 +60,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                );
+                ).addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
