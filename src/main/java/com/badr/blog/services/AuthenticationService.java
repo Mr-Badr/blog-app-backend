4eaefafb3263e07c9916ff87768e4667436c0883
Badr Eddine El-Harchali <Mr.Elharchali@gmail.com>
2025-05-24 19:13:49 +0100

    Add JwtAuthenticationFilter

--- src/main/java/com/badr/blog/services/AuthenticationService.java
+++ src/main/java/com/badr/blog/services/AuthenticationService.java
@@ -5,4 +5,5 @@ import org.springframework.security.core.userdetails.UserDetails;
 public interface AuthenticationService {
     UserDetails authenticate(String email, String password);
     String generateJwtToken(UserDetails userDetails);
+    UserDetails validateToken(String token);
 }
--- src/main/java/com/badr/blog/services/impl/AuthenticationServiceImpl.java
+++ src/main/java/com/badr/blog/services/impl/AuthenticationServiceImpl.java
@@ -50,6 +50,21 @@ public class AuthenticationServiceImpl implements AuthenticationService {
                 .compact();
     }
 
+    @Override
+    public UserDetails validateToken(String token) {
+        String username = extractUsername(token);
+        return userDetailsService.loadUserByUsername(username);
+    }
+
+    private String extractUsername(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
     private Key getSigningKey(){
         byte[] keyBytes = secretKey.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
